ae7a83b02e20297cec769471c2441da6b44a6c2f
Dylan Maassen van den Brink <dylan@mvdb.eu>
2023-04-17 16:31:45 +0200

    version bump

--- Marlin/src/inc/Version.h
+++ Marlin/src/inc/Version.h
@@ -25,7 +25,7 @@
  * Release version. Leave the Marlin version or apply a custom scheme.
  */
 #ifndef SHORT_BUILD_VERSION
-  #define SHORT_BUILD_VERSION "V1.2.1"
+  #define SHORT_BUILD_VERSION "V1.2.2"
 #endif
 /**
  * Verbose version identifier containing a unique identifier, such as the
